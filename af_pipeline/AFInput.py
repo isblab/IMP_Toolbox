7c397de887f926a38c4150c49873876b18644be4
omgol411 <omgolatkar2001@gmail.com>
2025-04-25 19:01:12 +0530

    added template settings addressing #44

--- af_pipeline/AFInput.py
+++ af_pipeline/AFInput.py
@@ -664,6 +664,38 @@ class Entity:
             entity_name=self.entity_name
         )
 
+    def get_template_settings(self):
+        """Get the template settings for the entity
+
+        - For proteinChain, get the template settings from the entity_info dictionary
+        - For dnaSequence, rnaSequence, ligand and ion, return an empty dictionary
+        - For proteinChain, if useStructureTemplate is not provided, set it to True
+        - For proteinChain, if maxTemplateDate is not provided, set it to 2021-09-30
+        - For proteinChain, if useStructureTemplate is False, ignore maxTemplateDate and raise a warning if maxTemplateDate is provided
+
+        Returns:
+            dict: template settings for the entity: maxTemplateDate, useStructureTemplate
+        """
+
+        template_dict = {}
+
+        if self.entity_type == "proteinChain":
+            if self.entity_info.get("useStructureTemplate", True):
+                template_dict = {
+                    "maxTemplateDate": self.entity_info.get("maxTemplateDate", "2021-09-30"),
+                    "useStructureTemplate": True
+                }
+            else:
+                if "maxTemplateDate" in self.entity_info:
+                    warnings.warn(
+                        f"maxTemplateDate is provided for {self.entity_name} but useStructureTemplate is False. Ignoring maxTemplateDate."
+                    )
+                template_dict = {
+                    "useStructureTemplate": False
+                }
+
+        return template_dict
+
     def get_entity_count(self)-> int:
         """Get the count of the entity
 
@@ -899,6 +931,7 @@ class Entity:
         self.start, self.end = self.get_entity_range()
         self.glycans = self.get_glycans()
         self.modifications = self.get_modifications()
+        self.template_settings = self.get_template_settings()
 
 
 class AFSequence(Entity):
@@ -978,6 +1011,8 @@ class AFSequence(Entity):
                     "count": self.count,
                 }
             }
+
+            af_sequence_dict[self.type].update(self.template_settings)
 
         elif self.type in ["dnaSequence", "rnaSequence"]:
             af_sequence_dict = {
