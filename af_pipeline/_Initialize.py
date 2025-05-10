3589e665116f5b1238565f0beea9e1a974f8f838
omgol411 <omgolatkar2001@gmail.com>
2025-05-10 11:43:36 +0530

    Update _Initialize addressing #54 and #62

--- af_pipeline/_Initialize.py
+++ af_pipeline/_Initialize.py
@@ -8,14 +8,16 @@ class _Initialize(AfParser):
     def __init__(
         self,
         data_file_path: str,
-        struct_file_path: str | None = None,
+        struct_file_path: str,
         af_offset: dict | None = None,
+        **kwargs,
     ):
 
         super().__init__(
             struct_file_path=struct_file_path,
             data_file_path=data_file_path,
             af_offset=af_offset,
+            **kwargs,
         )
 
         # AF2/3 structure file path.
@@ -23,6 +25,7 @@ class _Initialize(AfParser):
         # AF2/3 structure data path.
         self.data_file_path = data_file_path
         self.af_offset = af_offset
+        self.average_atom_pae = kwargs.get("average_atom_pae", False)
         self.get_attributes()
 
 
@@ -41,31 +44,63 @@ class _Initialize(AfParser):
         """
 
         data = self.dataparser.get_data_dict()
-        self.pae = self.dataparser.get_modified_pae(data=data)
+        self.pae = self.dataparser.get_pae(data=data)
         self.avg_pae = self.dataparser.get_avg_pae(pae=self.pae)
         self.contact_probs_mat = self.dataparser.get_contact_probs_mat(data=data)
+
         if self.contact_probs_mat is not None:
             self.avg_contact_probs_mat = self.dataparser.get_avg_contact_probs_mat(
                 contact_probs_mat=self.contact_probs_mat
             )
 
-        if self.struct_file_path:
+        self.token_chain_ids = self.dataparser.get_token_chain_ids(data=data)
+        self.token_res_ids = self.dataparser.get_token_res_ids(data=data)
+
+        if self.token_chain_ids is None or self.token_res_ids is None:
             self.token_chain_ids, self.token_res_ids = self.structureparser.get_token_chain_res_ids()
-            # Chain lengths for each chain.
-            self.lengths_dict = self.structureparser.get_chain_lengths(self.token_chain_ids)
-            # Ca-coords of all residues for each chain.
-            self.coords_list = self.structureparser.get_ca_coordinates()
-            # Ca-plddt of all residues for each chain.
-            self.plddt_list = self.structureparser.get_ca_plddt()
 
-        else:
-            self.token_chain_ids = self.dataparser.get_token_chain_ids(data=data)
-            self.token_res_ids = self.dataparser.get_token_res_ids(data=data)
-            self.lengths_dict = self.dataparser.get_chain_lengths(data=data)
+        # Ca-coords of all residues for each chain.
+        self.coords_list = self.structureparser.get_ca_coordinates()
+        # Ca-plddt of all residues for each chain.
+        self.plddt_list = self.structureparser.get_ca_plddt()
 
+        self.pae = self.update_pae(
+            pae=self.pae,
+            token_res_ids=self.token_res_ids,
+            token_chain_ids=self.token_chain_ids,
+            average_atom_pae=self.average_atom_pae,
+        )
+        self.avg_pae = self.update_pae(
+            pae=self.avg_pae,
+            token_res_ids=self.token_res_ids,
+            token_chain_ids=self.token_chain_ids,
+            average_atom_pae=self.average_atom_pae,
+        )
+        self.contact_probs_mat = self.update_contact_probs(
+            contact_probs_mat=self.contact_probs_mat,
+            token_chain_ids=self.token_chain_ids,
+            token_res_ids=self.token_res_ids,
+            average_atom_pae=self.average_atom_pae,
+        )
+        self.avg_contact_probs_mat = self.update_contact_probs(
+            contact_probs_mat=self.avg_contact_probs_mat,
+            token_chain_ids=self.token_chain_ids,
+            token_res_ids=self.token_res_ids,
+            average_atom_pae=self.average_atom_pae,
+        )
+        self.token_chain_ids, self.token_res_ids = self.update_token_ids(
+            token_chain_ids=self.token_chain_ids,
+            token_res_ids=self.token_res_ids,
+            average_atom_pae=self.average_atom_pae,
+        )
+
+        self.lengths_dict = self.get_chain_lengths(
+            token_chain_ids=self.token_chain_ids,
+        )
         self.sanity_check()
         self.idx_to_num, self.num_to_idx = self.renumber.residue_map(
-            self.token_chain_ids, self.token_res_ids
+            token_chain_ids=self.token_chain_ids,
+            token_res_ids=self.token_res_ids,
         )
 
 
