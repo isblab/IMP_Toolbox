6c717c8aa598446bdca4eb13fabca45f575af555
omgol411 <omgolatkar2001@gmail.com>
2025-10-03 14:44:34 +0530

    get bead name using IMP functions

--- analysis/rmf_to_xyzr.py
+++ analysis/rmf_to_xyzr.py
@@ -8,85 +8,35 @@ import IMP.atom
 import IMP.core
 import IMP.rmf
 import numpy as np
-from typing import Dict
 from collections import defaultdict
-from multiprocessing import Pool
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from utils import get_res_range_from_key, write_json
 import getpass
 _user = getpass.getuser()
 
-def process_frags(
-    frags: list,
-    frag_dict: Dict[str, list],
-    round_off: int | None = None,
-):
-    """ Process fragments to extract bead coordinates and radii.
-
-    - For flexible beads, XYZR is sotred at the coarse-grained level.
-      i.e. "1-10" : [[x, y, z, r], [x, y, z, r], ...]
-    - For rigid bodies, XYZR is stored at the residue level assuming
-      the highest resolution is 1 and each bead is a residue.
-      i.e. "1" : [[x, y, z, r], [x, y, z, r], ...]
+def get_bead_name(p):
+    """ Get bead name in the format:
+    `molName_copyIndex_resStart-resEnd` or `molName_copyIndex_resNum`
+    e.g. "ProteinA_0_1-10" or "ProteinA_0_11"
 
     Args:
-        frags (list):
-            List of fragment hierarchies.
-        frag_dict (Dict[str, list]):
-            Dictionary to store fragment data.
-        round_off (int | None, optional):
-            Decimal places to round off coordinates and radii.
+        p (IMP.Particle): Particle object.
 
     Returns:
-        Dict[str, list]:
-            Updated fragment dictionary with xyzr for each fragment.
+        str: Bead name.
     """
 
-    for frag in frags:
-        frag_name = frag.get_name()
-        beads_in_frag = frag.get_children()
-        assert len(beads_in_frag) == 1; (
-            f"Expected 1 bead in {frag_name}, found {len(beads_in_frag)}"
-        )
-        beads_in_frag = frag.get_children()[0].get_children()
+    mol_name = IMP.atom.get_molecule_name(IMP.atom.Hierarchy(p))
+    cp_idx=IMP.atom.get_copy_index(IMP.atom.Hierarchy(p))
 
-        if "Frag_" in frag_name: # flexible beads
+    if IMP.atom.Fragment.get_is_setup(p):
+        res_nums = IMP.atom.Fragment(p).get_residue_indexes()
+        bead_name = f"{mol_name}_{cp_idx}_{min(res_nums)}-{max(res_nums)}"
+    else:
+        res_num = str(IMP.atom.Residue(p).get_index())
+        bead_name = f"{mol_name}_{cp_idx}_{res_num}"
 
-            beads_radii = [
-                IMP.core.XYZR(bead).get_radius() for bead in beads_in_frag
-            ]
-            beads_coords = [
-                list(IMP.core.XYZR(bead).get_coordinates())
-                for bead in beads_in_frag
-            ]
-            beads_residues = [
-                bead.get_name().replace("_bead", "")
-                for bead in beads_in_frag
-            ]
-
-            for res_range, coord, radius in zip(
-                beads_residues, beads_coords, beads_radii
-            ):
-                if round_off is not None:
-                    coord = [round(x, round_off) for x in coord]
-                    radius = round(radius, round_off)
-
-                frag_dict[res_range].append(coord + [radius])
-
-        elif "frags:" in frag_name: # rigid bodies
-
-            for bead in beads_in_frag:
-                residue = bead.get_name()
-                coord = list(IMP.core.XYZR(bead).get_coordinates())
-                radius = IMP.core.XYZR(bead).get_radius()
-
-                if round_off is not None:
-                    coord = [round(x, round_off) for x in coord]
-                    radius = round(radius, round_off)
-
-                frag_dict[str(residue)].append(coord + [radius])
-
-    return frag_dict
-
+    return bead_name
 
 def batch_worker(
     frame_batch: np.ndarray,
@@ -112,7 +62,7 @@ def batch_worker(
             Dictionary of moleculewise XYZR data for the processed frames.
     """
 
-    mol_rmf_dict = defaultdict(lambda: defaultdict(list))
+    mol_rmf_dict = defaultdict(list)
     mdl = IMP.Model()
     rmf_data = RMF.open_rmf_file_read_only(rmf_path)
     hier = IMP.rmf.create_hierarchies(rmf_data, mdl)[0]
@@ -121,21 +71,21 @@ def batch_worker(
 
         IMP.rmf.load_frame(rmf_data, RMF.FrameID(frame_id))
         mdl.update()
-        _state = hier.get_children()[0]
-        molecules = _state.get_children()
+        sel = IMP.atom.Selection(hier, resolution=1)
 
-        for mol in molecules:
-            mol_name = mol.get_name()
-            copy_idx = IMP.atom.get_copy_index(IMP.atom.Hierarchy(mol))
-            mol_key = mol_name + f"_{copy_idx}"
+        for leaf in sel.get_selected_particles():
 
-            mol_rmf_dict[mol_key] = process_frags(
-                frags=mol.get_children(),
-                frag_dict=mol_rmf_dict[mol_key],
-                round_off=round_off,
-            )
+            p = IMP.core.XYZR(leaf)
+            coord = list(p.get_coordinates())
+            radius = p.get_radius()
 
-    mol_rmf_dict = {k: dict(v) for k, v in mol_rmf_dict.items()}
+            if round_off is not None:
+                coord = [round(x, round_off) for x in coord]
+                radius = round(radius, round_off)
+
+            bead_name = get_bead_name(leaf)
+            mol_rmf_dict[bead_name] = coord + [radius]
+
     mol_rmf_dict = dict(mol_rmf_dict)
 
     del rmf_data
@@ -154,7 +104,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--output_path",
-        default=f"/data/{_user}/imp_toolbox_test/analysis/molwise_xyzr.h5",
+        default=f"/data/{_user}/imp_toolbox_test/analysis/sampcon_extracted_frames_xyzr.h5",
         type=str,
         help="Path to save the output HDF5 file."
     )
@@ -166,7 +116,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--round_off",
-        default=3,
+        default=5,
         type=int,
         help="Decimal places to round off coordinates and radii."
     )
@@ -179,7 +129,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     start_t = time.perf_counter()
-    molwise_xyzr = defaultdict(lambda: defaultdict(list))
+    molwise_xyzr = defaultdict(list)
 
     mdl = IMP.Model()
     rmf_data = RMF.open_rmf_file_read_only(args.rmf_path)
@@ -195,34 +145,35 @@ if __name__ == "__main__":
 
     frame_batches = np.array_split(np.arange(num_frames), args.nproc)
 
-    with Pool(processes=args.nproc) as pool:
-        results = pool.starmap(
-            batch_worker,
-            [(batch, args.rmf_path, args.round_off) for batch in frame_batches]
-        )
+    with ProcessPoolExecutor(max_workers=args.nproc) as executor:
+        futures = [
+            executor.submit(batch_worker, batch, args.rmf_path, args.round_off)
+            for batch in frame_batches
+        ]
+        results = []
+        for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
+            results.append(future.result())
 
     for res in results:
-        for mol_name, frag_dict in res.items():
-            for frag_name, xyzr in frag_dict.items():
-                molwise_xyzr[mol_name][frag_name].extend(xyzr)
+        for bead_name, xyzr in res.items():
+            molwise_xyzr[bead_name].append(xyzr)
 
     lap_t = time.perf_counter()
     print(f"Time taken to parse XYZR: {lap_t - start_t} seconds")
 
     with h5py.File(args.output_path, "w") as f:
-        for mol_name, frag_dict in molwise_xyzr.items():
-            mol_grp = f.create_group(mol_name)
-            for frag_name, xyzr in frag_dict.items():
-                mol_grp.create_dataset(
-                    frag_name,
-                    data=np.array(xyzr, dtype=np.float32),
-                    compression="gzip",
-                    compression_opts=9
-                )
+
+        for bead_name, xyzr in molwise_xyzr.items():
+            f.create_dataset(
+                bead_name,
+                data=np.array(xyzr, dtype=np.float64),
+                compression="gzip",
+                compression_opts=9
+            )
 
     # write_json(
     #     args.output_path.replace(".h5", ".json"),
-    #     {k: dict(v) for k, v in molwise_xyzr.items()},
+    #     molwise_xyzr
     # )
 
     print(f"Total time taken: {time.perf_counter() - start_t} seconds")
