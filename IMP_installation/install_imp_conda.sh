018a0cb088a7818d4f63d6e2585e9dcd5d88f914
omgol411 <omgolatkar2001@gmail.com>
2026-02-14 14:34:45 +0530

    update imp installation script

--- IMP_installation/install_imp_conda.sh
+++ IMP_installation/install_imp_conda.sh
@@ -1,40 +1,115 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # user input
 
-conda_env_name=${1:?"Please specify the name of the conda environment."}
-install_path=${2:?"Please specify the path of the directory where you would like to install IMP."}
-ncores=${3:?"Please specify the number of cores you would like to use for the build."}
-install_mode=${4:?"Please specify the installation source: 'github' or 'tarball'."}
+usage() {
+	echo >&2 \
+	"usage: install_imp_conda.sh -e conda_env_name -i install_path -s install_mode -I imp_version [-c num_cores] [-P python_version] [-m custom_module_path] [-y] [-h]
+
+Required arguments:
+-e conda_env_name: the name of the conda environment to be created or used
+-i install_path: the path where IMP will be installed
+-s install_mode: 'github' or 'tarball'
+-I imp_version: IMP version to install (e.g., 2.24.0) or branch name if installing from github
+
+Optional arguments:
+-c num_cores: number of cores to use for compilation (default: half the number of cores available on the system)
+-P python_version: Python version to use for installation (e.g., 3.12)
+-m custom_module_path: the path to a custom IMP module that you want to install along with IMP.
+-y: flag to skip confirmation prompts and use default values for environment name and installation choices
+-h: display this message"
+}
+
+#defaults
+conda_env_name=
+install_path=
+install_mode=
+imp_version=
+ncores=$(($(nproc) / 2))
+python_version=3.12
+custom_module_path=""
+confirm_flag="false"
+
+if grep -q "Fedora" /etc/os-release; then
+
+    fedora_version=$(cat /etc/os-release | grep VERSION_ID | awk -F '=' '{print $2}') ;
+    cgal_dir="/usr/share/cmake/CGAL" ;
+
+else
+    echo "This is not a Fedora system" ;
+    echo "This installation script is designed for Fedora systems. Exiting..." ;
+    exit 1 ;
+
+fi
+
+while getopts "e:i:s:I:c:P:yhm:" opt;
+do
+    case "$opt" in
+        e) conda_env_name="$OPTARG" ;;
+        i) install_path="$OPTARG" ;;
+        s) install_mode="$OPTARG" ;;
+        I) imp_version="$OPTARG" ;;
+        c) ncores="$OPTARG" ;;
+        P) python_version="$OPTARG" ;;
+        m) custom_module_path="$OPTARG" ;;
+        y) confirm_flag="true" ;;
+        h) usage; exit 0 ;;
+        *) usage; exit 1 ;;
+    esac
+done
+
+if [ -z "$conda_env_name" ] || [ -z "$install_path" ] || [ -z "$install_mode" ] || [ -z "$imp_version" ]; then
+    echo "Missing required arguments. Please provide values for conda_env_name, install_path, install_mode, and imp_version."
+    usage
+    exit 1
+fi
+
 if [ "$install_mode" != "github" ] && [ "$install_mode" != "tarball" ]; then
     echo "Invalid installation mode. Please specify 'github' or 'tarball'."
     exit 1
 fi
-imp_version=${5:?"Please provide the IMP version to install (e.g., 2.23.0). or branch name if installing from github."}
 
 cwd_=$(pwd) ;
 install_path=$(realpath $install_path) ;
 
 # conda environment setup
 
-read -p """
-This script will install IMP from the source code.
+# Check for a flag to use the default value for env_choice
+if [[ "$confirm_flag" == "true" ]]; then
 
-A new conda environment $conda_env_name will be created for the installation.
-All the python dependencies required for IMP will be installed in this environment.
+    choice=$conda_env_name
 
-Please confirm the environment name if you want to continue?: """ choice
+else
+
+    read -p """
+    This script will install IMP from the source code.
+
+    A new conda environment $conda_env_name will be created for the installation.
+    All the python dependencies required for IMP will be installed in this environment.
+
+    Please confirm the environment name if you want to continue?: """ choice
+
+fi
 
 if [ $choice == $conda_env_name ]; then
 
     if conda info --envs | grep -q "^$choice\b" ; then
 
-        echo "environment already exists."
-        read -p """
+        # Check for a flag to use the default value for env_choice
+        if [[ "$confirm_flag" == "true" ]]; then
 
-        WARNING: The environment already exists.
+            env_choice="y"
 
-        Do you want to continue installation in the existing environment (y/n)?: """ env_choice
+        else
+            echo "environment already exists."
+            read -p """
+
+            WARNING: The environment already exists.
+
+            Do you want to continue installation in the existing environment (y/n)?: """ env_choice
+
+            read -p ""
+        fi
 
         case "$env_choice" in
 
@@ -52,30 +127,30 @@ if [ $choice == $conda_env_name ]; then
             echo "Installing IMP dependencies in existing conda environment $conda_env_name" ;
             source ~/.bashrc ;
             conda activate $conda_env_name ;
-            conda install python=3.12 matplotlib numpy scipy scikit-learn -y ;
+            conda install python=$python_version matplotlib numpy scipy scikit-learn -y ;
         fi
 
     else
         echo "Installing IMP dependencies in a new conda environment $conda_env_name" ;
 
-        conda create -n $conda_env_name python=3.12 matplotlib numpy scipy scikit-learn -y ;
+        conda create -n $conda_env_name python=$python_version matplotlib numpy scipy scikit-learn -y ;
         source ~/.bashrc ;
 
         if conda info --envs | grep -q "^$choice\b" ; then
-            echo "environment exists."
+            echo "environment exists." ;
         else
-            echo "There was an error creating the conda environment. Exiting..."
-            exit 1
+            echo "There was an error creating the conda environment. Exiting..." ;
+            exit 1 ;
         fi
     fi
 
 else
     echo "The environment name does not match. Exiting..." ;
-    exit 1
+    exit 1 ;
 
 fi
 
-echo $CONDA_DEFAULT_ENV
+echo $CONDA_DEFAULT_ENV ;
 
 # install required system packages
 
@@ -102,7 +177,7 @@ cd $install_path/imp-clean/ ;
 if [ $install_mode == "tarball" ]; then
     echo "Downloading IMP $imp_version tarball";
 
-    tarball_name="imp-$imp_version"
+    tarball_name="imp-$imp_version" ;
 
     if [[ -f "$tarball_name.tar.gz" ]]; then
         echo "The IMP tarball already exists, will not download again.";
@@ -111,15 +186,15 @@ if [ $install_mode == "tarball" ]; then
         wget https://integrativemodeling.org/$imp_version/download/$tarball_name.tar.gz ;
         wget https://integrativemodeling.org/$imp_version/download/SHA256SUM ;
         if [ $? -ne 0 ]; then
-            echo "Error downloading the IMP tarball. Please check the version number and your internet connection."
-            exit 1
+            echo "Error downloading the IMP tarball. Please check the version number and your internet connection." ;
+            exit 1 ;
         fi
         python $cwd_/check_sha256sum.py --file_path $tarball_name.tar.gz --sha256_file SHA256SUM ;
         if [ $? -eq 0 ]; then
-            echo "SHA256 checksum verification passed."
+            echo "SHA256 checksum verification passed." ;
         else
-            echo "SHA256 checksum verification failed. Exiting."
-            exit 1
+            echo "SHA256 checksum verification failed. Exiting." ;
+            exit 1 ;
         fi
         wait 2 ;
     fi
@@ -137,23 +212,23 @@ if [ $install_mode == "tarball" ]; then
 elif [ $install_mode == "github" ]; then
 
     if [[ -d "./imp" ]] ; then
-        whether_to_install="no"
-        echo "The imp repository already exists. Checking version.";
-        github_imp_version=$(cat $install_path/imp-clean/imp/VERSION)
+        whether_to_install="no" ;
+        echo "The imp repository already exists. Checking version." ;
+        github_imp_version=$(cat $install_path/imp-clean/imp/VERSION) ;
 
         if [ "$github_imp_version" == "$imp_version" ]; then
-            echo "The cloned GitHub IMP version ($github_imp_version) matches the requested version ($imp_version). Will not clone again.";
-            whether_to_install="no"
+            echo "The cloned GitHub IMP version ($github_imp_version) matches the requested version ($imp_version). Will not clone again." ;
+            whether_to_install="no" ;
 
         else
-            echo "The cloned GitHub IMP version ($github_imp_version) does not match the requested version ($imp_version). Re-cloning the repository.";
-            whether_to_install="yes"
+            echo "The cloned GitHub IMP version ($github_imp_version) does not match the requested version ($imp_version). Re-cloning the repository." ;
+            whether_to_install="yes" ;
             sudo rm -rf imp ;
 
         fi
 
     else
-        whether_to_install="yes"
+        whether_to_install="yes" ;
     fi
 
     if [ "$whether_to_install" == "yes" ]; then
@@ -166,8 +241,8 @@ elif [ $install_mode == "github" ]; then
             git clone --recurse-submodules --branch $imp_version https://github.com/salilab/imp.git ;
         }
         if [ $? -ne 0 ]; then
-            echo "Error cloning the IMP repository. Please check your internet connection or the repository URL."
-            exit 1
+            echo "Error cloning the IMP repository. Please check your internet connection or the repository URL." ;
+            exit 1 ;
         fi
         wait 2 ;
 
@@ -187,42 +262,57 @@ fi
 
 # custom module installation
 
-read -p "Do you want to install your own module and recompile IMP (y/n)?" choice
+if [[ -n "$custom_module_path" ]]; then
 
-case "$choice" in
-y|Y ) echo "yes"; custom_module_install="yes";;
-n|N ) echo "no"; custom_module_install="no";;
-* ) echo "invalid"; custom_module_install="no";;
-esac
+    echo "Custom module path provided as an argument. Will attempt to install the custom module from the provided path." ;
+
+    if [[ ! -d $custom_module_path ]]; then
+        echo "The custom module path does not exist. Exiting..." ;
+        exit 1 ;
+    fi
+
+    custom_module_install="yes" ;
+
+else
+
+    read -p "Do you want to install your own module and recompile IMP (y/n)?" choice
+
+    case "$choice" in
+    y|Y ) echo "yes"; custom_module_install="yes";;
+    n|N ) echo "no"; custom_module_install="no";;
+    * ) echo "invalid"; custom_module_install="no";;
+    esac
+
+    if [ $custom_module_install == "yes" ]; then
+
+        read -p "Please provide the full path to the custom module: " custom_module_path
+    
+        if [[ ! -d $custom_module_path ]]; then
+            echo "The custom module path does not exist. Exiting..." ;
+            exit 1 ;
+        fi
+    fi
+
+fi
 
 if [ $custom_module_install == "yes" ]; then
 
-    echo "Installing custom module and recompiling IMP";
+    echo "Installing custom module and recompiling IMP" ;
 
     cd $imp_src_path/modules ;
 
-    read -p "Please provide the full path to the custom module: " custom_module_path
-
-    if [[ ! -d $custom_module_path ]]; then
-        echo "The custom module path does not exist. Exiting..."
-        exit 1
-    fi
-
-    echo "Copying custom module to imp-clean"
+    echo "Copying custom module to imp-clean" ;
 
     if [[ -d $imp_src_path/modules/$(basename $custom_module_path) ]]; then
-        echo "The custom module already exists in imp"
-        read -p "Do you want to overwrite the existing module (y/n)?" choice
-        case "$choice" in
-          y|Y ) echo "yes";;
-          n|N ) echo "no"; exit 1;;
-          * ) echo "invalid"; exit 1;;
-        esac
-        rm -rf $imp_src_path/modules/$(basename $custom_module_path)
+        echo "The custom module already exists in imp" ;
+        echo "Removing the existing custom module from imp for fresh installation" ;
+        rm -rf $imp_src_path/modules/$(basename $custom_module_path) ;
     fi
 
-    cp -r $custom_module_path $imp_src_path/modules
+    cp -r $custom_module_path $imp_src_path/modules ;
 fi
+
+echo "Custom module installation complete." ;
 
 # Recompiling IMP
 
@@ -235,20 +325,13 @@ fi
 mkdir build ;
 cd build ;
 
-if grep -q "Fedora" /etc/os-release; then
-
-    fedora_version=$(cat /etc/os-release | grep VERSION_ID | awk -F '=' '{print $2}')
-    cgal_dir="/usr/share/cmake/CGAL"
-
-else
-    echo "This is not a Fedora system"
-    exit 1
-
+if [ "$$CONDA_DEFAULT_ENV" != "$conda_env_name" ]; then
+    conda activate $conda_env_name ;
 fi
-
-conda activate $conda_env_name
 
 cmake $imp_src_path -DCMAKE_CXX_COMPILER=mpic++ -DCMAKE_C_COMPILER=mpicc -DCMAKE_BUILD_TYPE=Release -DIMP_MAX_CHECKS=NONE -DIMP_MAX_LOG=SILENT -G=Ninja -DCGAL_DIR=$cgal_dir ;
 
 echo "Compiling IMP" ;
-ninja -j $ncores 1> recompile.log 2> recompile.err;
+ninja -j $ncores 1> recompile.log 2> recompile.err ;
+
+sudo chown -R $USER:$USER $install_path/imp-clean ;
