a09ba4b4c6b3a1f05d62a0d1d365ec9e242bcfae
omgol411 <omgolatkar2001@gmail.com>
2026-02-25 13:46:40 +0530

    making module - added structure related scripts

--- IMP_Toolbox/sequence/Sequence.py
+++ IMP_Toolbox/sequence/Sequence.py
@@ -15,123 +15,118 @@ from IMP_Toolbox.constants.imp_toolbox_constants import (
     MiscStrEnum,
 )
 
-class FetchSequences:
-    """Fetch sequences for given uniprot ids and save them in fasta format """
+def query_uniprot_api_for_sequences(
+    uniprot_ids: list,
+    max_retries: int = 3
+) -> str:
+    """ Get sequences for given uniprot ids in fasta format
 
-    def __init__(self, uniprot_ids):
-        self.uniprot_ids = uniprot_ids
+    ## Arguments:
 
-    def query_uniprot_api_for_sequences(self, max_retries: int = 3) -> str:
-        """ Get sequences for given uniprot ids in fasta format
+    - **max_retries (int, optional):**:<br />
+        Number of times to retry the request in case of failure. Defaults to 3.
 
-        ## Arguments:
+    ## Returns:
 
-        - **max_retries (int, optional):**:<br />
-            Number of times to retry the request in case of failure. Defaults to 3.
+    - **str**:<br />
+        Fasta formatted string containing sequences for the given uniprot ids
+    """
 
-        ## Returns:
+    joined = ",".join(uniprot_ids)
+    req_sess = request_session(max_retries=max_retries)
+    response = req_sess.get(
+        APIurl.uniprot_rest_api_sequence.substitute(ids=joined)
+    )
 
-        - **str**:<br />
-            Fasta formatted string containing sequences for the given uniprot ids
-        """
+    if response.status_code == 200:
+        print("Successfully fetched sequences for given uniprot ids")
+        return response.text
 
-        joined = ",".join(self.uniprot_ids)
-        req_sess = request_session(max_retries=max_retries)
-        response = req_sess.get(
-            APIurl.uniprot_rest_api_sequence.substitute(ids=joined)
+    else:
+        raise Exception("Error while requesting sequences for given uniprot ids")
+
+def only_uniprot_id_as_header(
+    fasta_str: str | None = None,
+    fasta_file: str | None = None,
+) -> str:
+    """ Keep only uniprot id as header in the fasta file
+
+    Assumes the fasta headers are in the format:
+    >sp|P12345|PROT_HUMAN Some description here
+
+    ## Arguments:
+
+    - **fasta_str (str, optional):**:<br />
+        Fasta formatted string containing sequences.
+
+    - **fasta_file (str, optional):**:<br />
+        Path to fasta file containing sequences.
+
+    ## Returns:
+
+    - **str**:<br />
+        Fasta formatted string containing sequences for the given uniprot
+        ids with only uniprot id as header
+    """
+
+    if (
+        (fasta_str is None and fasta_file is None) or
+        (fasta_str is not None and fasta_file is not None)
+    ):
+        raise ValueError(
+            """
+            Only one of the following should be provided:
+            - fasta_str (fasta formatted string containing sequences for the given uniprot ids)
+            - fasta_file (path to fasta file containing sequences for the given uniprot ids)
+            """
         )
 
-        if response.status_code == 200:
-            print("Successfully fetched sequences for given uniprot ids")
-            return response.text
+    if fasta_file is not None:
+        with open(fasta_file, "r") as f:
+            fasta_str = f.read()
 
+    sequences_lines = fasta_str.split("\n")
+    for i, line in enumerate(sequences_lines):
+        if line.startswith(">"):
+            sequences_lines[i] = f">{line.split('|')[1]}"
+
+    fasta = "\n".join(sequences_lines)
+
+    return fasta
+
+def fasta_str_to_dict(fasta_str: str) -> dict:
+    """ Convert a FASTA string to a dictionary of sequences.
+
+    ## Arguments:
+
+    - **fasta_str (str)**:<br />
+        FASTA string
+
+    ## Returns:
+
+    - **dict**:<br />
+        Dictionary of sequences.
+
+    ## Examples:
+
+    >>> fasta_str = '''>seq1
+    ... ABCD
+    ... >seq2
+    ... ABCD'''
+
+    >>> fasta_str_to_dict(fasta_str)
+    {'seq1': 'ABCD', 'seq2': 'ABCD'}
+    """
+
+    fasta_str = fasta_str.splitlines()
+    fasta_dict = {}
+    for i, line in enumerate(fasta_str):
+        if line.startswith(">"):
+            seq_name = line[1:].strip()
+            fasta_dict[seq_name] = ""
         else:
-            raise Exception("Error while requesting sequences for given uniprot ids")
-
-    @staticmethod
-    def only_uniprot_id_as_header(
-        fasta_str: str | None = None,
-        fasta_file: str | None = None,
-    ) -> str:
-        """ Keep only uniprot id as header in the fasta file
-
-        Assumes the fasta headers are in the format:
-        >sp|P12345|PROT_HUMAN Some description here
-
-        ## Arguments:
-
-        - **fasta_str (str, optional):**:<br />
-            Fasta formatted string containing sequences.
-
-        - **fasta_file (str, optional):**:<br />
-            Path to fasta file containing sequences.
-
-        ## Returns:
-
-        - **str**:<br />
-            Fasta formatted string containing sequences for the given uniprot
-            ids with only uniprot id as header
-        """
-
-        if (
-            (fasta_str is None and fasta_file is None) or
-            (fasta_str is not None and fasta_file is not None)
-        ):
-            raise ValueError(
-                """
-                Only one of the following should be provided:
-                - fasta_str (fasta formatted string containing sequences for the given uniprot ids)
-                - fasta_file (path to fasta file containing sequences for the given uniprot ids)
-                """
-            )
-
-        if fasta_file is not None:
-            with open(fasta_file, "r") as f:
-                fasta_str = f.read()
-
-        sequences_lines = fasta_str.split("\n")
-        for i, line in enumerate(sequences_lines):
-            if line.startswith(">"):
-                sequences_lines[i] = f">{line.split('|')[1]}"
-
-        fasta = "\n".join(sequences_lines)
-
-        return fasta
-
-    @staticmethod
-    def fasta_str_to_dict(fasta_str: str) -> dict:
-        """ Convert a FASTA string to a dictionary of sequences.
-
-        ## Arguments:
-
-        - **fasta_str (str)**:<br />
-            FASTA string
-
-        ## Returns:
-
-        - **dict**:<br />
-            Dictionary of sequences.
-
-        ## Examples:
-
-        >>> fasta_str = '''>seq1
-        ... ABCD
-        ... >seq2
-        ... ABCD'''
-
-        >>> fasta_str_to_dict(fasta_str)
-        {'seq1': 'ABCD', 'seq2': 'ABCD'}
-        """
-
-        fasta_str = fasta_str.splitlines()
-        fasta_dict = {}
-        for i, line in enumerate(fasta_str):
-            if line.startswith(">"):
-                seq_name = line[1:].strip()
-                fasta_dict[seq_name] = ""
-            else:
-                fasta_dict[seq_name] += line.strip()
-        return fasta_dict
+            fasta_dict[seq_name] += line.strip()
+    return fasta_dict
 
 class PairwiseSequenceAlignment:
     """ Handle pairwise sequence alignment from two sequences. """
@@ -336,7 +331,7 @@ class PairwiseSequenceAlignment:
             )
 
         if isinstance(pairwise_alignment, psa.PairwiseAlignment):
-            pairwise_alignment_dict = FetchSequences.fasta_str_to_dict(
+            pairwise_alignment_dict = fasta_str_to_dict(
                 fasta_str=pairwise_alignment.fasta()
             )
 
---
+++ IMP_Toolbox/structure/BestStructure.py
@@ -0,0 +1,142 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+from IMP_Toolbox.utils_imp_toolbox.api_helpers import (
+    request_session,
+    request_result
+)
+from IMP_Toolbox.utils_imp_toolbox.file_helpers import (
+    read_json,
+    write_json
+)
+from IMP_Toolbox.constants.imp_toolbox_constants import (
+    APIurl,
+)
+from IMP_Toolbox.constants.structure_constants import BestStructureCol
+from IMP_Toolbox.constants.sequence_constants import UNIPROT_ISOFORM_SEPARATOR
+
+def get_best_structures(uniprot_id: str) -> dict:
+    """ Get the best structures for a given uniprot id.
+
+    ## Arguments:
+
+    - **uniprot_id (str)**:<br />
+        The uniprot id for which to fetch the best structures.
+
+    ## Returns:
+
+    - **dict**:<br />
+        A dictionary containing the best structures for the given uniprot id.
+    """
+
+    req_sess = request_session(max_retries=3)
+    get_request = req_sess.get(
+        url=f"{APIurl.pdbe_api_best_structures}/{uniprot_id}"
+    )
+
+    best_structures = request_result(
+        get_request, uniprot_id, ignore_error=True
+    )
+
+    return best_structures
+
+def make_best_structures_df(
+    best_structures: dict,
+    uniprot_protein_map: dict = {},
+) -> pd.DataFrame:
+    """ Create a dataframe of best structures from dictionary
+
+    ## Arguments:
+
+    - **best_structures (dict)**:<br />
+        The best structures dictionary returned by `get_best_structures`.
+
+    - **uniprot_protein_map (dict, optional):**:<br />
+        Mapping from uniprot ids to protein names. Defaults to {}.
+
+    ## Returns:
+
+    - **pd.DataFrame**:<br />
+        A dataframe containing the best structures for the given uniprot ids.
+    """
+
+    all_best_chains = []
+
+    for uniprot_id, best_structure in best_structures.items():
+
+        if not best_structure:
+            print(f"No best structure found for {uniprot_id}")
+
+        best_chains = best_structure[uniprot_id.split("-")[0]]
+        best_chains = [
+            chain for chain in best_chains
+            if chain[BestStructureCol.RESOLUTION] is not None
+        ]
+        best_chains = sorted(
+            best_chains, key=lambda x: x[BestStructureCol.RESOLUTION]
+        )
+
+        for _, chain in enumerate(best_chains):
+
+            all_best_chains.append(
+            {
+                BestStructureCol.UNIPROT_ID: uniprot_id,
+                BestStructureCol.PROTEIN: uniprot_protein_map.get(uniprot_id, ""),
+                BestStructureCol.CHAIN_ID: chain[BestStructureCol.CHAIN_ID],
+                BestStructureCol.PDB_ID: chain[BestStructureCol.PDB_ID],
+                BestStructureCol.COVERAGE: chain[BestStructureCol.COVERAGE],
+                BestStructureCol.UNP_START: chain[BestStructureCol.UNP_START],
+                BestStructureCol.UNP_END: chain[BestStructureCol.UNP_END],
+                BestStructureCol.START: chain[BestStructureCol.START],
+                BestStructureCol.END: chain[BestStructureCol.END],
+                BestStructureCol.RESOLUTION: chain[BestStructureCol.RESOLUTION],
+            }
+        )
+
+    df = pd.DataFrame(all_best_chains)
+    mask = df.duplicated(subset=[BestStructureCol.UNIPROT_ID], keep='first')
+    df[BestStructureCol.UNIPROT_ID] = df[BestStructureCol.UNIPROT_ID].mask(mask, '')
+    df[BestStructureCol.PROTEIN] = df[BestStructureCol.PROTEIN].mask(mask, '')
+    df = df.drop_duplicates(subset=[BestStructureCol.PDB_ID], keep='first')
+
+    return df
+
+def fetch_best_structures(
+    uniprot_ids: list,
+    save_path: str,
+    overwrite: bool=False,
+) -> dict:
+    """ Fetch best structures for given proteins.
+
+    ## Arguments:
+
+    - **uniprot_ids (list)**:<br />
+        A list of uniprot ids for which to fetch the best structures.
+
+    - **save_path (str)**:<br />
+        The path where the best structures dictionary will be saved as a json file.
+
+    - **overwrite (bool, optional):**:<br />
+        Whether to overwrite the existing json file if it already exists. Defaults to False.
+
+    ## Returns:
+
+    - **dict**:<br />
+        A dictionary containing the best structures for the given uniprot ids.
+    """
+
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+    if os.path.exists(save_path) and overwrite == False:
+        best_structures = read_json(save_path)
+
+    else:
+        best_structures = {}
+        for uniprot_id in tqdm(uniprot_ids):
+            # uniprot_id = uniprot_id.split("-")[0]
+            best_structures[uniprot_id] = get_best_structures(
+                uniprot_id.split(UNIPROT_ISOFORM_SEPARATOR)[0]
+            )
+        write_json(save_path, best_structures)
+
+    return best_structures
