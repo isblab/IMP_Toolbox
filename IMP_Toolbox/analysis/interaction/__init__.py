dc92b385864afc7cc9bf1f8fec552cea577a194f
omgol411 <omgolatkar2001@gmail.com>
2026-05-13 19:48:39 +0530

    skip submodules while import

--- IMP_Toolbox/analysis/interaction/__init__.py
+++ IMP_Toolbox/analysis/interaction/__init__.py
@@ -6,3 +6,12 @@ interaction
   - coarse-grained interactions (e.g., IMP-derived models)
   - atomic interactions (e.g., Arpeggio or COCOMAPS 2 analysis of PDB structures)
 """
+
+from .coarse_grained.interaction_map import (
+    read_molecules_from_file,
+    read_molecule_pairs_from_file,
+    Molecules,
+    MoleculePairs,
+    PairwiseMaps,
+    Interaction,
+)
--- IMP_Toolbox/structure/__init__.py
+++ IMP_Toolbox/structure/__init__.py
@@ -7,3 +7,19 @@ structure
   - fetching density maps from EMDB
   - comparing density maps
 """
+
+from .burial import (
+    get_burial_info,
+    get_residue_depth,
+)
+
+from .best_structure import (
+    get_best_structures,
+    fetch_best_structures,
+    make_best_structures_df,
+)
+
+from .split import (
+    split_structure_by_chain,
+    get_per_chain_residues,
+)
