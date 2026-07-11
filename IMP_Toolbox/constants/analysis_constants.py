6954238918c7e6970ec29b68e79eb59ff0c20f2e
omgol411 <omgolatkar2001@gmail.com>
2026-07-11 13:03:52 +0530

    extend regex pattern for molecule name

--- IMP_Toolbox/constants/analysis_constants.py
+++ IMP_Toolbox/constants/analysis_constants.py
@@ -4,7 +4,8 @@ PAIR_SEP = "|"
 RES_RANGE_SEP = "-"
 MOL_COPY_SEP = "_"
 MOL_RANGE_SEP = ":"
-REGEX_MOLNAME = r'^([A-Za-z0-9]+)(?:_(\d+))?(?::([\d-]+))?$'
+# REGEX_MOLNAME = r'^([A-Za-z0-9]+)(?:_(\d+))?(?::([\d-]+))?$'
+REGEX_MOLNAME = r'^([A-Za-z0-9]+)(?:_(\d+))?(?::(\d+(?:-\d+)?(?:,\d+(?:-\d+)?)*)?)?$'
 EXPECTED_MOLNAME_FORMATS = [
     "MOL_COPYIDX:RESSTART-RESEND",
     "MOL_COPYIDX",
