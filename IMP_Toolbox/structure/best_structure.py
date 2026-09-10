544dc5cd8f48f476cba2853ae9489a574d45ef22
omgol411 <omgolatkar2001@gmail.com>
2026-09-10 14:16:40 +0530

    minor reorganization

--- IMP_Toolbox/structure/best_structure.py
+++ IMP_Toolbox/structure/best_structure.py
@@ -16,61 +16,6 @@ from IMP_Toolbox.constants.imp_toolbox_constants import (
 from IMP_Toolbox.constants.structure_constants import BestStructureCol
 from IMP_Toolbox.constants.sequence_constants import UNIPROT_ISOFORM_SEPARATOR
 
-def get_best_structures(uniprot_id: str) -> dict:
-    """ Get the best structures for a given uniprot id.
-
-    ## Arguments:
-
-    - **uniprot_id (str)**:<br />
-        The uniprot id for which to fetch the best structures.
-
-    ## Returns:
-
-    - **dict**:<br />
-        A dictionary containing the best structures for the given uniprot id.
-    """
-
-    req_sess = request_session(max_retries=3)
-    get_request = req_sess.get(
-        url=APIurl.pdbe_api_best_structures.substitute(uniprot_id=uniprot_id)
-    )
-
-    best_structures = request_result(
-        get_request, uniprot_id, ignore_error=True
-    )
-
-    return best_structures
-
-def get_polymer_coverage(pdb_id: str, chain_id: str) -> dict:
-    """ Get the polymer coverage for a given pdb id and chain id.
-
-    ## Arguments:
-
-    - **pdb_id (str)**:<br />
-        The pdb id for which to fetch the polymer coverage.
-
-    - **chain_id (str)**:<br />
-        The chain id for which to fetch the polymer coverage.
-
-    ## Returns:
-
-    - **dict**:<br />
-        A dictionary containing the polymer coverage for the given pdb id and chain id.
-    """
-
-    req_sess = request_session(max_retries=3)
-    get_request = req_sess.get(
-        url=APIurl.pdbe_api_poly_coverage.substitute(
-            pdb_id=pdb_id, chain_id=chain_id
-        )
-    )
-
-    polymer_coverage = request_result(
-        get_request, f"{pdb_id}_{chain_id}", ignore_error=True
-    )
-
-    return polymer_coverage
-
 def make_best_structures_df(
     best_structures: dict,
     protein_uniprot_map: dict = {},
@@ -137,7 +82,146 @@ def make_best_structures_df(
 
     return df
 
-def get_modeled_coverage(
+def get_best_structures(uniprot_id: str) -> dict:
+    """ Get the best structures for a given uniprot id.
+    See the correspondoing fetch function :func:`fetch_best_structures`
+
+    ## Arguments:
+
+    - **uniprot_id (str)**:<br />
+        The uniprot id for which to fetch the best structures.
+
+    ## Returns:
+
+    - **dict**:<br />
+        A dictionary containing the best structures for the given uniprot id.
+    """
+
+    req_sess = request_session(max_retries=3)
+    get_request = req_sess.get(
+        url=APIurl.pdbe_api_best_structures.substitute(uniprot_id=uniprot_id)
+    )
+
+    best_structures = request_result(
+        get_request, uniprot_id, ignore_error=True
+    )
+
+    return best_structures
+
+def get_polymer_coverage(pdb_id: str, chain_id: str) -> dict:
+    """ Get the polymer coverage for a given pdb id and chain id.
+    See the correspondoing fetch function :func:`fetch_polymer_coverage`
+
+    ## Arguments:
+
+    - **pdb_id (str)**:<br />
+        The pdb id for which to fetch the polymer coverage.
+
+    - **chain_id (str)**:<br />
+        The chain id for which to fetch the polymer coverage.
+
+    ## Returns:
+
+    - **dict**:<br />
+        A dictionary containing the polymer coverage for the given pdb id and chain id.
+    """
+
+    req_sess = request_session(max_retries=3)
+    get_request = req_sess.get(
+        url=APIurl.pdbe_api_poly_coverage.substitute(
+            pdb_id=pdb_id, chain_id=chain_id
+        )
+    )
+
+    polymer_coverage = request_result(
+        get_request, f"{pdb_id}_{chain_id}", ignore_error=True
+    )
+
+    return polymer_coverage
+
+def fetch_best_structures(
+    uniprot_ids: list,
+    save_path: str,
+    overwrite: bool=False,
+) -> dict:
+    """ Fetch best structures for given proteins.
+
+    ## Arguments:
+
+    - **uniprot_ids (list)**:<br />
+        A list of uniprot ids for which to fetch the best structures.
+
+    - **save_path (str)**:<br />
+        The path where the best structures dictionary will be saved as a json file.
+
+    - **overwrite (bool, optional):**:<br />
+        Whether to overwrite the existing json file if it already exists. Defaults to False.
+
+    ## Returns:
+
+    - **dict**:<br />
+        A dictionary containing the best structures for the given uniprot ids.
+    """
+
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+    if os.path.exists(save_path) and overwrite == False:
+        best_structures = read_json(save_path)
+
+    else:
+        best_structures = {}
+        for uniprot_id in tqdm(uniprot_ids):
+            uniprot_base = uniprot_id.split(UNIPROT_ISOFORM_SEPARATOR)[0]
+            best_structures[uniprot_id] = get_best_structures(uniprot_base)
+        write_json(save_path, best_structures)
+
+    best_structures = add_modeled_coverage(
+        best_structures=best_structures,
+        outdir=os.path.join(os.path.dirname(save_path), "polymer_coverage")
+    )
+
+    return best_structures
+
+def fetch_polymer_coverage(
+    pdb_id: str,
+    chain_id: str,
+    save_path: str,
+    overwrite: bool=False,
+) -> dict:
+    """ Fetch polymer coverage for given pdb id and chain id.
+
+    ## Arguments:
+
+    - **pdb_id (str)**:<br />
+        The pdb id for which to fetch the polymer coverage.
+
+    - **chain_id (str)**:<br />
+        The chain id for which to fetch the polymer coverage.
+
+    - **save_path (str)**:<br />
+        The path where the polymer coverage dictionary will be saved as a json file.
+
+    - **overwrite (bool, optional):**:<br />
+        Whether to overwrite the existing json file if it already exists. Defaults to False.
+
+    ## Returns:
+
+    - **dict**:<br />
+        A dictionary containing the polymer coverage for the given pdb id and chain id.
+    """
+
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+    if os.path.exists(save_path) and overwrite == False:
+        polymer_coverage = read_json(save_path)
+
+    else:
+        polymer_coverage = get_polymer_coverage(pdb_id, chain_id)
+        write_json(save_path, polymer_coverage)
+
+    return polymer_coverage
+
+def calculate_modeled_coverage(
     pdb_id: str,
     chain_id: str,
     uniprot_length: int,
@@ -209,7 +293,7 @@ def add_modeled_coverage(
             pdb_id = chain[BestStructureCol.PDB_ID]
             chain_id = chain[BestStructureCol.CHAIN_ID]
 
-            coverage, modeled_residues = get_modeled_coverage(
+            coverage, modeled_residues = calculate_modeled_coverage(
                 pdb_id=pdb_id,
                 chain_id=chain_id,
                 uniprot_length=seq_lengths.get(uniprot_id, 1),
@@ -219,88 +303,5 @@ def add_modeled_coverage(
                 BestStructureCol.MODELED_COVERAGE: f"{coverage:.2f}",
                 BestStructureCol.MODELED_RESIDUES: modeled_residues
             })
-
-    return best_structures
-
-def fetch_polymer_coverage(
-    pdb_id: str,
-    chain_id: str,
-    save_path: str,
-    overwrite: bool=False,
-) -> dict:
-    """ Fetch polymer coverage for given pdb id and chain id.
-
-    ## Arguments:
-
-    - **pdb_id (str)**:<br />
-        The pdb id for which to fetch the polymer coverage.
-
-    - **chain_id (str)**:<br />
-        The chain id for which to fetch the polymer coverage.
-
-    - **save_path (str)**:<br />
-        The path where the polymer coverage dictionary will be saved as a json file.
-
-    - **overwrite (bool, optional):**:<br />
-        Whether to overwrite the existing json file if it already exists. Defaults to False.
-
-    ## Returns:
-
-    - **dict**:<br />
-        A dictionary containing the polymer coverage for the given pdb id and chain id.
-    """
-
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-
-    if os.path.exists(save_path) and overwrite == False:
-        polymer_coverage = read_json(save_path)
-
-    else:
-        polymer_coverage = get_polymer_coverage(pdb_id, chain_id)
-        write_json(save_path, polymer_coverage)
-
-    return polymer_coverage
-
-def fetch_best_structures(
-    uniprot_ids: list,
-    save_path: str,
-    overwrite: bool=False,
-) -> dict:
-    """ Fetch best structures for given proteins.
-
-    ## Arguments:
-
-    - **uniprot_ids (list)**:<br />
-        A list of uniprot ids for which to fetch the best structures.
-
-    - **save_path (str)**:<br />
-        The path where the best structures dictionary will be saved as a json file.
-
-    - **overwrite (bool, optional):**:<br />
-        Whether to overwrite the existing json file if it already exists. Defaults to False.
-
-    ## Returns:
-
-    - **dict**:<br />
-        A dictionary containing the best structures for the given uniprot ids.
-    """
-
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-
-    if os.path.exists(save_path) and overwrite == False:
-        best_structures = read_json(save_path)
-
-    else:
-        best_structures = {}
-        for uniprot_id in tqdm(uniprot_ids):
-            # uniprot_id = uniprot_id.split("-")[0]
-            uniprot_base = uniprot_id.split(UNIPROT_ISOFORM_SEPARATOR)[0]
-            best_structures[uniprot_id] = get_best_structures(uniprot_base)
-        write_json(save_path, best_structures)
-
-    best_structures = add_modeled_coverage(
-        best_structures=best_structures,
-        outdir=os.path.join(os.path.dirname(save_path), "polymer_coverage")
-    )
 
     return best_structures
